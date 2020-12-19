469e7cbfdce73ea5b629abf1c2c3d333495b1068
Andriy Rudkovsky <a.rudkovskyy@gmail.com>
2020-12-19 23:52:16 +0200

    solves task 11-1

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+}
+
 const colors = [
   '#FFFFFF',
   '#2196F3',
@@ -7,6 +13,34 @@ const colors = [
   '#795548',
 ];
 
+const NOTIFICATION_DELAY = 1000;
+let intervalId = null;
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+refs.startBtn.addEventListener('click', setRandomColor);
+
+refs.stopBtn.addEventListener('click', stopSetRandomColor);
+
+function setRandomColor() {
+  if (intervalId) {
+    return;
+  }
+  intervalId = setInterval(() => {
+    const randomColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+    // console.log(randomColor);
+    refs.body.style.backgroundColor = randomColor;
+  }, NOTIFICATION_DELAY);
+}
+
+function stopSetRandomColor() {
+  if (!intervalId) {
+    return;
+  }
+  clearInterval(intervalId);
+  intervalId = null;
+  // console.log('Stop changing background color, please!');
+}
+
